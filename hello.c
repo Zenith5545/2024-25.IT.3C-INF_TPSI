876642934f899fc26c48ddaff273040f14413257
Zenith5545 <davide.saporiti@outlook.it>
2024-12-16 07:58:23 +0000

    save 4

--- hello.c
+++ hello.c
@@ -62,3 +62,61 @@ int main() {
 
     printf("\n\n");
 }
+
+
+
+int main2() {
+   
+    int A, B, C, D, E, F;
+
+printf("Inserisci il valore di A (0 o 1):\n");
+scanf("%d", &A);                                // and
+printf("Inserisci il valore di B (0 o 1):\n");
+scanf("%d", &B);
+
+printf("Inserisci il valore di C (0 o 1):\n");
+scanf("%d", &C);                               // not
+
+printf("Inserisci il valore di D (0 o 1):\n");
+scanf("%d", &D);    
+                                               // or
+printf("Inserisci il valore di E (0 o 1):\n");
+scanf("%d", &E);
+
+printf("Inserisci il valore di F (0 o 1):\n");
+scanf("%d", &F);                               //not         
+
+
+
+
+int AND(int A; int B){
+    return A * B
+}
+
+int NOT(int C) {
+    return 1 - C;
+}
+
+int OR(int D, int F) {
+    return (D + E) - (D * E);
+}
+
+int NOT2(int F) {
+    return 1 - F;
+}
+
+
+
+
+
+int OR2(int AND, int AND) {
+    return (AND + AND) - (AND * AND);
+
+int AND2(int OR; int NOT2){
+    return OR * NOT2
+}
+
+
+
+
+}
