055473c2bf55cb71a32f25a6057432180d7a12cc
Zenith5545 <davide.saporiti@outlook.it>
2025-05-28 21:16:02 +0000

    save 22

--- hello2.c
+++ hello2.c
@@ -1,67 +1,117 @@
-#include <stdio.h>
-
-// Funzione per simulare la porta NOT
-int NOT(int a) {
-    // Il valore di ritorno della funzione
-    return 1 - a;
-}
-
-// Funzione per simulare la porta AND
-int AND(int a, int B) {
-    // Il valore di ritorno della funzione
-    // TODO aggiungere l'espressione matematica per soddisfare la porta AND
-    return a * B;
-}   // 1337 = LEET
-
-// Funzione per simulare la porta OR
-int OR(int a, int B) {
-    // Il valore di ritorno della funzione
-    // TODO aggiungere l'espressione matematica per soddisfare la porta OR
-    return (a + B) - (a * B);
-}
-
-// Funzine per simulare la porta EX-OR
-int EXOR(int a, int b){
-    // Il valore di ritorno della funzione
-    // TODO aggiungere l'espressione matematica per soddisfare la porta AND
-return (a+b)-((2*a)*b);
-}
-
-
-    int main() {
-        FILE *file;
-        int num[10]; // Buffer per leggere 10 interi
-        size_t result;
-        int i;
+void LETTURA(char* reg, int indiceREG, int ValoreReg[8]) {
+    
+    for (int b = 0; b < 8; b++) {
+        int r = indiceREG + b;
         
-        // Aprire il file in modalità di lettura
-        file = fopen("in.txt", "r");
-
-
-        if (file == NULL) {
-            perror("Errore nell'apertura del file: file non trovato");
-            return 1;
+        if (reg[r] == '0' || reg[r] == '1') {
+            ValoreReg[b] = reg[r] - '0';
+        } else {
+            ValoreReg[b] = 0; 
         }
-    
-        // Leggere 10 interi dal file
-        result = fread(num, sizeof(int), 10, file);
+    }
+}
 
 
-        if (result != 10) {
-            perror("Errore nella lettura dal file: file non leggibile");
-            fclose(file);
-            return 1;
-        }
-    
-        // Stampare i numeri letti
-        printf("Numeri letti dal file:\n");
-        for (i = 0; i < 10; i++) {
-            printf("%d\n", num[i]);
-        }
-    
-        // Chiudere il file
-        fclose(file);
+
+
+
+
+
+
+
+
+int main() {
+
+
+
+
+
+//Dichiarazione variabili
+    int lunghezza;
+    int InRegA[4][8]; 
+    int OutRegA[4][8];
+
+    int InRegB[4][8]; 
+    int OutRegB[4][8];
+
+
+    lunghezza = strlen(numA);
+
+    printf("Numero inserito: %s (numero di bit %d)\n", numA, lunghezza);
+    if(lunghezza > 32){ 
+        printf("valore A causa overflow\n");
         return 0;
     }
 
 
+
+    lunghezza = strlen(numB);
+
+    printf("Numero inserito: %s (numero di bit %d)\n", numB, lunghezza);
+    if(lunghezza > 32){ 
+        printf("valore B causa overflow\n");
+        return 0;
+    }
+
+
+
+
+
+//Memorizzazione nel registro A e B
+    for (int r = 0; r < 4; r++) { 
+        LETTURA(numA, r * 8, InRegA[r]);
+
+printf("Inizializzazione e Interazione con Registro %d\n", r);
+        REGISTRO(InRegA[r][0], InRegA[r][1], InRegA[r][2], InRegA[r][3],
+                 InRegA[r][4], InRegA[r][5], InRegA[r][6], InRegA[r][7],
+                 &OutRegA[r][0], &OutRegA[r][1], &OutRegA[r][2], &OutRegA[r][3],
+                 &OutRegA[r][4], &OutRegA[r][5], &OutRegA[r][6], &OutRegA[r][7]);
+
+
+        LETTURA(numB, r * 8, InRegB[r]);
+
+printf("Inizializzazione e Interazione con Registro %d\n", r);
+        REGISTRO(InRegB[r][0], InRegB[r][1], InRegB[r][2], InRegB[r][3],
+                 InRegB[r][4], InRegB[r][5], InRegB[r][6], InRegB[r][7],
+                 &OutRegB[r][0], &OutRegB[r][1], &OutRegB[r][2], &OutRegB[r][3],
+                 &OutRegB[r][4], &OutRegB[r][5], &OutRegB[r][6], &OutRegB[r][7]);
+
+    }
+
+
+//Visualizzazione risultato
+    printf("Stato finale del registro A:\n");
+    for (int r = 0; r < 4; r++){
+        printf("Registro %d: ", r);
+        for (int b = 0; b < 8; b++) {
+            printf("%d", OutRegA[r][b]);
+        }
+        printf("\n");
+    }
+
+        printf("Stato finale del registro B:\n");
+    for (int r = 0; r < 4; r++){
+        printf("Registro %d: ", r);
+        for (int b = 0; b < 8; b++) {
+            printf("%d", OutRegB[r][b]);
+        }
+        printf("\n");
+    }
+
+
+    //sezione calcolo
+
+ALU();
+ALU();
+ALU();
+ALU();
+ALU();
+ALU();
+ALU();
+ALU();
+
+
+
+    printf("\nProgramma terminato\n");
+    return 0;
+}
